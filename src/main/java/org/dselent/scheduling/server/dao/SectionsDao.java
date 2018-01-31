73e120f0c7b627348459b52f5cd01befa491c31b
itsmcnutty <evanphilip11@yahoo.com>
2018-01-31 14:09:20 -0500

    Added Section DAO

---
+++ src/main/java/org/dselent/scheduling/server/dao/SectionsDao.java
@@ -0,0 +1,10 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.Section;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectionsDao extends Dao<Section>
+{
+	// add functions here as needed
+}
---
+++ src/main/java/org/dselent/scheduling/server/extractor/SectionsExtractor.java
@@ -0,0 +1,43 @@
+package org.dselent.scheduling.server.extractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dselent.scheduling.server.model.Section;
+
+public class SectionsExtractor extends Extractor<List<Section>>
+{
+	@Override
+	public List<Section> extractData(ResultSet rs) throws SQLException
+	{
+		List<Section> resultList = new ArrayList<>();
+
+		while(rs.next())
+		{
+			Section result = new Section();
+				
+			result.setId(rs.getInt(Section.getColumnName(Section.Columns.ID)));
+			
+			if(rs.wasNull())
+			{
+				result.setId(null);
+			}
+			
+			result.setName(rs.getString(Section.getColumnName(Section.Columns.NAME)));						//
+			result.setType(rs.getString(Section.getColumnName(Section.Columns.TYPE)));
+			result.setCrn(rs.getString(Section.getColumnName(Section.Columns.CRN)));
+			result.setExpectedPopulation(rs.getString(Section.getColumnName(Section.Columns.EXPECTED_POPULATION)));
+			result.setCourseId(rs.getString(Section.getColumnName(Section.Columns.COURSE_ID)));
+			result.setCalendarId(rs.getString(Section.getColumnName(Section.Columns.CALENDAR_ID)));
+			result.setScheduleId(rs.getString(Section.getColumnName(Section.Columns.SCHEDULE_ID)));
+			result.setCreatedAt(rs.getTimestamp(Section.getColumnName(Section.Columns.CREATED_AT)));
+		
+			resultList.add(result);
+		}
+			
+		return resultList;
+	}
+
+}
