e3fe75e81af5a6054798345111a11f75ca34a0d9
ywang20 <ywang20@wpi.edu>
2018-02-02 23:08:38 -0500

    final version

--- src/test/java/org/dselent/scheduling/server/dao/CoursesHistoryDaoTest.java
+++ src/test/java/org/dselent/scheduling/server/dao/CoursesHistoryDaoTest.java
@@ -8,6 +8,7 @@ import org.dselent.scheduling.server.config.AppConfig;
 import org.dselent.scheduling.server.dao.CalendarDao;
 import org.dselent.scheduling.server.miscellaneous.Pair;
 import org.dselent.scheduling.server.model.Calendar;
+import org.dselent.scheduling.server.model.CourseHistory;
 import org.dselent.scheduling.server.sqlutils.ColumnOrder;
 import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
 import org.dselent.scheduling.server.sqlutils.QueryTerm;
@@ -19,21 +20,39 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {AppConfig.class})
+@ContextConfiguration(classes = { AppConfig.class })
 @WebAppConfiguration
 public class CoursesHistoryDaoTest {
 	@Autowired
 	private CoursesHistoryDao coursesHistoryDao;
-	
+
 	/*
-	 * Not really an using this as a JUnit test
-	 * More of an example on how to use the classes
+	 * Not really an using this as a JUnit test More of an example on how to use the
+	 * classes
 	 */
-    @Test
-    public void CoursesHistoryDao() throws SQLException
-    {
-    	// INSERT
-    	
-    }
-    
+	@Test
+	public void CoursesHistoryDao() throws SQLException {
+		String selectColumnName = CourseHistory.getColumnName(CourseHistory.Columns.COURSE_ID);
+		Integer selectCourseId = 1;
+
+		List<QueryTerm> selectQueryTermList = new ArrayList<>();
+
+		QueryTerm selectUseNameTerm = new QueryTerm();
+		selectUseNameTerm.setColumnName(selectColumnName);
+		selectUseNameTerm.setComparisonOperator(ComparisonOperator.EQUAL);
+		selectUseNameTerm.setValue(selectCourseId);
+		selectQueryTermList.add(selectUseNameTerm);
+
+		List<String> selectColumnNameList = CourseHistory.getColumnNameList();
+
+		List<Pair<String, ColumnOrder>> orderByList = new ArrayList<>();
+		Pair<String, ColumnOrder> orderPair1 = new Pair<String, ColumnOrder>(selectColumnName, ColumnOrder.ASC);
+		orderByList.add(orderPair1);
+
+		@SuppressWarnings("unused")
+		List<CourseHistory> selectedUserList = coursesHistoryDao.select(selectColumnNameList, selectQueryTermList, orderByList);
+
+		System.out.println(selectedUserList);
+	}
+
 }
